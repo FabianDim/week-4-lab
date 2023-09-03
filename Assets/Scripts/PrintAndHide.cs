079d3dc4ea6c95fdf954d31e3f176fb92792a701
FabianDim <63860191+FabianDim@users.noreply.github.com>
2023-09-03 18:38:12 +1000

    Hide the thi

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -8,15 +8,25 @@ public class PrintAndHide : MonoBehaviour
 
     private int i = 3;
     public Renderer rend;
+    private int rand;
         
     void Start()
     {
-        
+        rand = Random.Range(150, 250);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         Debug.Log(gameObject.name + ++i);
+        if (GameObject.FindWithTag("Red") && i >= 100)
+        {
+            GameObject.FindWithTag("Red").SetActive(false);  
+        }
+
+        if (GameObject.FindWithTag("Blue") && i == rand)
+        {
+            rend.enabled = false;
+        }
     }
 }
